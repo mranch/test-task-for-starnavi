94cab3df8a935e93bc1511e1683b752161aee4b9
mranch <anchiforov@gmail.com>
2019-10-04 16:13:44 +0300

    Tests added - final version

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import pytest
+
+necessary_result = [11, 55, 15, 21, 44, 32, 13, 25, 43]
+
+
 def read_matrix():
     with open('input.txt') as input_matrix:
         return [[x for x in r.split()] for r in input_matrix]
 
 
-def set_route_recurrence(matrix, start=11):
+def set_route_recurrence(matrix=read_matrix(), start=11):
     current = start
 
     def next_step_recurrence():
@@ -19,11 +24,12 @@ def set_route_recurrence(matrix, start=11):
         else:
             current = next_step_recurrence()
             return create_result_list(current_list)
+
     return create_result_list
 
 
 class TestTask:
-    def __init__(self, start=None, matrix=read_matrix()):
+    def __init__(self, start=11, matrix=read_matrix()):
         TestTask.res = [start]
         TestTask.start = start
         TestTask.matrix = matrix
@@ -44,11 +50,13 @@ class TestTask:
         return self.res
 
 
-if __name__ == '__main__':
-    result = read_matrix()
-    testTask = TestTask(start=11)
-    with open('output_oop.txt', 'w') as output_oop:
-        output_oop.write(" ".join(str(x) for x in testTask.set_route))
-    func = set_route_recurrence(result)
-    with open('output_recurrence.txt', 'w') as output_recurrence_result:
-        output_recurrence_result.write(" ".join(str(r) for r in func()))
+@pytest.mark.parametrize("expected", [necessary_result])
+def test_set_route_oop(expected):
+    vvv = TestTask()
+    assert expected == vvv.set_route
+
+
+@pytest.mark.parametrize("expected", [necessary_result])
+def test_set_route_fp(expected):
+    func = set_route_recurrence()
+    assert expected == func()
